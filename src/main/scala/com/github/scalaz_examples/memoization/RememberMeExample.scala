8bd04304524ec9cdab6f71fcfc3af05b393bb746
Capwell <capwed@usxxpoddap1m1.corp.emc.com>
2014-03-24 20:07:05 -0700

    examples of NonEmptyList and Memo

---
+++ src/main/scala/com/github/scalaz_examples/memoization/RememberMeExample.scala
@@ -0,0 +1,58 @@
+package com.github.scalaz_examples.memoization
+
+import com.google.common.cache.{CacheBuilder, Cache}
+import java.util.concurrent.{TimeUnit, Callable}
+import scalaz._
+import Scalaz._
+import com.google.common.base.Stopwatch
+
+object RememberMeExample extends App {
+  // we have all used a cache before, its a very simple idea: something takes a while to do, so save it in a faster
+  // medium for quicker lookup.  If you use Java, you probably know Guava.  If you use Guava often, you probably
+  // know Guava's Cache.
+  def longRunning(input: Int): Int = {
+    Thread.sleep(1000) // takes a long time!
+    input * input
+  }
+  import scala.language.implicitConversions
+  implicit def toCallable[U](unit: => U): Callable[U] = new Callable[U] {
+    override def call(): U = unit
+  }
+  def timed[U](work: => U): U = {
+    val sw = Stopwatch.createStarted()
+    val data = work
+    println(s"Timed(${sw.elapsed(TimeUnit.MILLISECONDS)}}): ${data}")
+    data
+  }
+  val cache: Cache[Int, Int] = CacheBuilder.
+    newBuilder().
+    build().
+    asInstanceOf[Cache[Int, Int]] // required cause of issues with scalac
+  timed(cache.get(1, longRunning(1)))
+  timed(cache.get(1, longRunning(1)))
+
+  // thats great and all, and we were able to make the cache less code with implicits, but I rather not have to deal
+  // with it unless I need to
+  // memo returns K => V, so need to set it as a val
+  val longRunningMemo = Memo.immutableHashMapMemo(longRunning)
+
+  // with immutableMapMemo, cacheing is now built into the function
+  timed(longRunningMemo(1))
+  timed(longRunningMemo(1))
+  timed(longRunningMemo(2))
+  timed(longRunningMemo(2))
+
+  // can even combine the both if you want a more powerful cache
+  val longRunningMemoGuava = Memo.memo {method: (Int => Int) =>
+    val cache: Cache[Int, Int] = CacheBuilder.
+      newBuilder().
+      build().
+      asInstanceOf[Cache[Int, Int]]
+
+    def ret(key: Int): Int = cache.get(key, method(key))
+    ret
+  }(longRunning)
+
+  timed(longRunningMemoGuava(1))
+  timed(longRunningMemoGuava(1))
+}
---
+++ src/main/scala/com/github/scalaz_examples/util/NelExample.scala
@@ -0,0 +1,22 @@
+package com.github.scalaz_examples.util
+
+import scalaz._
+import Scalaz._
+
+object NelExample extends App {
+  // have you ever been bitten by getting an empty list and getting the 0th element or head?
+  // thats what NonEmptyList (nel) tries to make more clear.
+  // Nel exists to make a function more clear when the list returned/passed in can never be empty
+
+  val list = NonEmptyList(1, 2, 3)
+  list.println
+
+  list.println
+
+  // cons elements into the lists
+  (4 <:: list).println
+  (list :::> List(4)).println
+
+  // returns List[Int], since the tail size is unknown
+  list.tail.println
+}
